73463010eaf07c563feda47856ab2cca6968a3a8
Carlos1980E <167463536+Carlos1980E@users.noreply.github.com>
2024-04-18 21:03:34 +0200

    Update Hello.java

--- Programacion/Hello.java
+++ Programacion/Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
 	public static void main(String [] args){
 		System.out.println("Hola Mundo!!!");
+		for(int i = 0; i < 5; i++){
+			System.out.println("Probando más Cosas");
+		}
 	}
 }
